79361a37aa1c3504310a78e7cfb7655f6f7dd716
Camilo Rodriguez <>
2018-09-19 17:33:35 -0500

    0.1

---
+++ src/ahorcado/Palabra.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Palabra {
+   String Palabras []={"cacao","dolor","carne","afecto","nacer","morir","vivir","obeso","raton","radio","volar","calvo","soñar"};
+   Void EscojerPalabra(){
+       Random MiPalabraA= new Random();
+       
+   }
+}

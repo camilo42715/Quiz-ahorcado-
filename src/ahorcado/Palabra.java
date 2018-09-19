3e3d3566d5e6576ce9d63b9b4a409b309f3c8325
Camilo Rodriguez <Estudiantes@Labing70608.udistrital.edu.co>
2018-09-19 17:58:21 -0500

    0.2

--- src/ahorcado/Palabra.java
+++ src/ahorcado/Palabra.java
@@ -12,10 +12,24 @@ import java.util.Random;
  * @author Estudiantes
  */
 public class Palabra {
-   String Palabras []={"cacao","dolor","carne","afecto","nacer","morir","vivir","obeso","raton","radio","volar","calvo","soñar"};
-   Void EscojerPalabra(){
-       Random MiPalabraA= new Random();
-       return null;
-       
-   }
+
+    String Palabras[] = {"cacao", "dolor", "carne", "afecto", "nacer", "morir", "vivir", "obeso", "raton", "radio", "volar", "calvo", "soñar"};
+
+    String palabraEscogida;
+    String EscojerPalabra() {
+        Random Aleatoria = new Random();
+        int posicion = Aleatoria.nextInt(11);
+        palabraEscogida= Palabras[posicion];
+        return Palabras[posicion];
+    }
+
+    int numerodeceldas() {
+        
+        char letra=palabraEscogida.charAt(4);
+         return palabraEscogida.length();
+         
+         
+    }
 }
+
+
